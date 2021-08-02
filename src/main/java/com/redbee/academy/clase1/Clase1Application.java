df63b8b9d09f172661c6cfaf20def95a31a0dd5a
Diego Navarrete <diego.navarrete92@gmail.com>
2021-08-02 09:14:18 -0300

    Change application class name

--- src/main/java/com/redbee/academy/clase1/Clase1Application.java
+++ src/main/java/com/redbee/academy/clase1/Clase1Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ChallengeApplication {
+public class Clase1Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(ChallengeApplication.class, args);
+        SpringApplication.run(Clase1Application.class, args);
     }
 
 }
